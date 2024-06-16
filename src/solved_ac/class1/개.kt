52217aee1960225b43ea4fbba37ba37b2aced69c
osy <oks153123@naver.com>
2024-06-17 02:08:03 +0900

    개 문제풀이
    https://www.acmicpc.net/problem/31403

---
+++ src/solved_ac/class1/개.kt
@@ -0,0 +1,20 @@
+package src.solved_ac.class1
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/10172">개</a>
+ *
+ * 그래 내가 개다
+ *
+ * 개 짖는 소리 좀 안나게 하라
+ * */
+fun main() {
+    val dog = """
+        |\_/|
+        |q p|   /}
+        ( 0 )""${'"'}\
+        |"^"`    |
+        ||_/=\\__|
+    """.trimIndent()
+
+    println(dog)
+}
