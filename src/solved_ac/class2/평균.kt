dd9ac9bb80a2a31733637a5e7e143cd6a1532933
osy <oks153123@naver.com>
2024-06-22 03:29:33 +0900

    평균 문제풀이
    https://www.acmicpc.net/problem/1546

---
+++ src/solved_ac/class2/평균.kt
@@ -0,0 +1,27 @@
+package src.solved_ac.class2
+
+import java.util.*
+import kotlin.math.max
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/1546">평균</a>
+ *
+ * 세준이는 자기 점수 중에 최댓값을 골랐다. 이 값을 M이라고 한다. 그리고 나서 모든 점수를 점수/M*100으로 고쳤다.
+ *
+ * 예를 들어, 세준이의 최고점이 70이고, 수학점수가 50이었으면 수학점수는 50/70*100이 되어 71.43점이 된다.
+ *
+ * 세준이의 성적을 위의 방법대로 새로 계산했을 때, 새로운 평균을 구하는 프로그램을 작성하시오.
+ * */
+fun main() {
+    val scanner = Scanner(System.`in`.bufferedReader())
+    val count = scanner.nextInt()
+
+    var max = 0
+    var sum = 0
+    repeat(count) {
+        val nextInt = scanner.nextInt()
+        max = max(max, nextInt)
+        sum += nextInt
+    }
+    println(sum.toFloat() / max * 100 / count)
+}
