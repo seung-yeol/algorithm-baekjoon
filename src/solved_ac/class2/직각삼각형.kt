91754eb814e229c94f3089c0a7f7c3aad70b9cf9
osy <oks153123@naver.com>
2024-07-14 01:14:34 +0900

    직각삼각형 문제풀이
    https://www.acmicpc.net/problem/4153

---
+++ src/solved_ac/class2/직각삼각형.kt
@@ -0,0 +1,25 @@
+package src.solved_ac.class2
+
+import java.util.*
+
+/**
+ * @see <a href="https://www.acmicpc.net/problem/4153">직각삼각형</a>
+ *
+ * 과거 이집트인들은 각 변들의 길이가 3, 4, 5인 삼각형이 직각 삼각형인것을 알아냈다. 주어진 세변의 길이로 삼각형이 직각인지 아닌지 구분하시오.
+ * */
+fun main() {
+    val buffer = System.`in`.bufferedReader()
+    val scanner = Scanner(buffer)
+    val stringBuilder = StringBuilder()
+
+    while (true) {
+        val first = scanner.nextInt().run { this * this }.takeIf { it != 0 } ?: break
+        val second = scanner.nextInt().run { this * this }
+        val third = scanner.nextInt().run { this * this }
+
+        val isTriangle = first + second + third == maxOf(first, second, third) * 2
+        stringBuilder.appendLine(if (isTriangle) "right" else "wrong")
+    }
+
+    println(stringBuilder)
+}
