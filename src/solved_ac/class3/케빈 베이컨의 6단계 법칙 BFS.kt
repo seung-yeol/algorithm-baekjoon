b5ed9ffe70eda964f833e1c023603726b980b212
osy <oks153123@naver.com>
2024-10-13 14:51:46 +0900

    케빈 베이컨의 6단계 법칙(BFS) 문제풀이
    https://www.acmicpc.net/problem/30804

---
+++ src/solved_ac/class3/케빈 베이컨의 6단계 법칙 BFS.kt
@@ -0,0 +1,85 @@
+package solved_ac.class3
+
+import java.io.BufferedReader
+
+/**
+ * **문제**
+ * - 케빈 베이컨의 6단계 법칙에 의하면 지구에 있는 모든 사람들은 최대 6단계 이내에서 서로 아는 사람으로 연결될 수 있다.
+ * - 케빈 베이컨 게임은 임의의 두 사람이 최소 몇 단계 만에 이어질 수 있는지 계산하는 게임이다.
+ * - 케빈 베이컨은 미국 헐리우드 영화배우들 끼리 케빈 베이컨 게임을 했을때 나오는 단계의 총 합이 가장 적은 사람이라고 한다.
+ * - 유저 중에서 케빈 베이컨의 수가 가장 작은 사람을 찾으려고 한다.
+ * - 케빈 베이컨 수는 모든 사람과 케빈 베이컨 게임을 했을 때, 나오는 단계의 합이다.
+ * - BOJ 유저의 수와 친구 관계가 입력으로 주어졌을 때, 케빈 베이컨의 수가 가장 작은 사람을 구하는 프로그램을 작성하시오.
+ *
+ * **예시**
+ * - BOJ의 유저가 5명이고, 1과 3, 1과 4, 2와 3, 3과 4, 4와 5가 친구인 경우를 생각해보자.
+ * - 1은 2까지 3을 통해 2단계 만에, 3까지 1단계, 4까지 1단계, 5까지 4를 통해서 2단계 만에 알 수 있다. 따라서, 케빈 베이컨의 수는 2+1+1+2 = 6이다.
+ * - 2는 1까지 3을 통해서 2단계 만에, 3까지 1단계 만에, 4까지 3을 통해서 2단계 만에, 5까지 3과 4를 통해서 3단계 만에 알 수 있다. 따라서, 케빈 베이컨의 수는 2+1+2+3 = 8이다.
+ * - 3은 1까지 1단계, 2까지 1단계, 4까지 1단계, 5까지 4를 통해 2단계 만에 알 수 있다. 따라서, 케빈 베이컨의 수는 1+1+1+2 = 5이다.
+ * - 4는 1까지 1단계, 2까지 3을 통해 2단계, 3까지 1단계, 5까지 1단계 만에 알 수 있다. 4의 케빈 베이컨의 수는 1+2+1+1 = 5가 된다.
+ * - 5는 1까지 4를 통해 2단계, 2까지 4와 3을 통해 3단계, 3까지 4를 통해 2단계, 4까지 1단계 만에 알 수 있다. 5의 케빈 베이컨의 수는 2+3+2+1 = 8이다.
+ * - 5명의 유저 중에서 케빈 베이컨의 수가 가장 작은 사람은 3과 4이다.
+ *
+ * **입력**
+ * - 첫째 줄에 유저의 수 N (2 ≤ N ≤ 100)과 친구 관계의 수 M (1 ≤ M ≤ 5,000)이 주어진다.
+ * - 둘째 줄부터 M개의 줄에는 친구 관계가 주어진다. 친구 관계는 중복되어 들어올 수도 있으며, 친구가 한 명도 없는 사람은 없다.
+ * - 또, 모든 사람은 친구 관계로 연결되어져 있다.
+ * - 사람의 번호는 1부터 N까지이며, 두 사람이 같은 번호를 갖는 경우는 없다.
+ *
+ * **출력**
+ * - 첫째 줄에 BOJ의 유저 중에서 케빈 베이컨의 수가 가장 작은 사람을 출력한다. 그런 사람이 여러 명일 경우에는 번호가 가장 작은 사람을 출력한다.
+ *
+ * @see <a href="https://www.acmicpc.net/problem/1389">케빈 베이컨의 6단계 법칙</a>
+ * */
+fun main() {
+    val bufferedReader = System.`in`.bufferedReader()
+    val (count, line) = bufferedReader.nextInts()
+    val connections = Array(count + 1) { mutableSetOf<Int>() }
+
+    repeat(line) {
+        val (a, b) = bufferedReader.nextInts()
+        connections[a].add(b)
+        connections[b].add(a)
+    }
+
+    val solver = Q1389(connections)
+    println(solver.answer())
+}
+
+private class Q1389(val connections: Array<MutableSet<Int>>) {
+    private var answer = 0
+    private var min = Int.MAX_VALUE
+
+    fun answer(): Int {
+        for (index in 1..connections.lastIndex) {
+            val visited = BooleanArray(connections.size + 1) { false }.apply { set(index, true) }
+            val newKevinBacon = bfs(index = index, step = 1, visited)
+
+            if (min <= newKevinBacon) continue
+            min = newKevinBacon
+            answer = index
+        }
+
+        return answer
+    }
+
+    private fun bfs(index: Int, step: Int, visited: BooleanArray): Int {
+        var connectedCount = 0
+        val queue = LinkedHashSet<Int>()
+        for (i in connections[index]) {
+            if (visited[i]) continue
+
+            visited[i] = true
+            queue.add(i)
+            connectedCount += step
+        }
+
+        for (i in queue) {
+            connectedCount += bfs(i, step + 1, visited)
+        }
+
+        return connectedCount
+    }
+}
+
+private fun BufferedReader.nextInts(): IntArray = readLine().split(" ").map(String::toInt).toIntArray()
